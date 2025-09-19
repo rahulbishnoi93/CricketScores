1ce41283590f6412d61078bb282ed1cade474f6e
rahulbishnoi93 <rahulbishnoi93@gmail.com>
2025-09-19 11:48:22 +0530

    optimized libraries

--- PhoneApp/build.gradle.kts
+++ PhoneApp/build.gradle.kts
@@ -50,18 +50,13 @@ android {
 dependencies {
     // Core Android dependencies
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     
     // Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     
     // Lifecycle
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
     implementation("androidx.lifecycle:lifecycle-service:2.9.4")
     
@@ -78,13 +73,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     
     // Testing
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
     implementation(libs.core.splashscreen)
 
     implementation("com.google.android.gms:play-services-wearable:19.0.0")
--- WearApp/build.gradle.kts
+++ WearApp/build.gradle.kts
@@ -44,30 +44,20 @@ dependencies {
 
     implementation(libs.play.services.wearable)
     implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.compose.material)
     implementation(libs.compose.foundation)
-    implementation(libs.wear.tooling.preview)
     implementation(libs.activity.compose)
     implementation(libs.core.splashscreen)
     implementation(libs.compose.material3)
     implementation(libs.horologist.compose.layout)
     // Compose for Wear OS Dependencies
     implementation(libs.wear.compose.material)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
     // Compose preview annotations for Wear OS.
-    implementation(libs.androidx.compose.ui.tooling)
     // Compose for Wear OS Dependencies
-    implementation(libs.wear.compose.material)
 
     // Foundation is additive, so you can use the mobile version in your Wear OS app.
     implementation(libs.wear.compose.foundation)
     implementation(libs.androidx.material.icons.extended)
+    implementation("androidx.compose.material3:material3")
 
 
     // Retrofit
@@ -76,11 +66,8 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     // Navigation for Compose
-    implementation("androidx.wear.compose:compose-navigation:1.5.1")
+    implementation("androidx.wear.compose:compose-navigation")
     // Material3 (for PullToRefreshBox)
-    implementation("androidx.compose.material3:material3")
-
-    implementation("androidx.core:core-splashscreen:1.2.0-rc01")
 
 
 }
